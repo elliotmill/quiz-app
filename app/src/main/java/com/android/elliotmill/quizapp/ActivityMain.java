34451818bdfc9263a24c11444ccb6c475c28eee4
Azeez Olaniran <azeezolaniran2016@gmail.com>
2017-09-16 13:36:10 +0100

    Complete logic implementation for processing questions and generating scores and implement result ui

--- app/src/main/java/com/android/elliotmill/quizapp/ActivityMain.java
+++ app/src/main/java/com/android/elliotmill/quizapp/ActivityMain.java
@@ -2,12 +2,207 @@ package com.android.elliotmill.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class ActivityMain extends AppCompatActivity {
+import com.android.elliotmill.quizapp.questions.Question;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ActivityMain extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
+    private ProgressBar pbQuestion;
+    private TextView tvProgress, tvOptA, tvOptB, tvOptC, tvOptD, tvQuestion, tvScore;
+    private CheckBox cbOptA, cbOptB, cbOptC, cbOptD;
+    private int currentQuestion;
+    private Button btnNext, btnComplete;
+    private View layoutQuestions, layoutReport;
+    private ArrayList<Question> questionsList;
+    private boolean[] scoreTracker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        currentQuestion = 1;
+
+        questionsList = Question.generateAllQuestions();
+        scoreTracker = new boolean[questionsList.size()];
+
         setContentView(R.layout.activity_main);
+        pbQuestion = (ProgressBar)findViewById(R.id.pbQuestions);
+        tvProgress = (TextView)findViewById(R.id.tvProgress);
+        updateQuestionProgress();
+
+        tvQuestion = (TextView)findViewById(R.id.tvQuestion);
+        tvScore = (TextView)findViewById(R.id.tvScore);
+
+        tvOptA = (TextView)findViewById(R.id.tvOptA);
+        tvOptB = (TextView)findViewById(R.id.tvOptB);
+        tvOptC = (TextView)findViewById(R.id.tvOptC);
+        tvOptD = (TextView)findViewById(R.id.tvOptD);
+
+        cbOptA = (CheckBox)findViewById(R.id.cbOptA);
+        cbOptA.setOnCheckedChangeListener(this);
+        cbOptB = (CheckBox)findViewById(R.id.cbOptB);
+        cbOptB.setOnCheckedChangeListener(this);
+        cbOptC = (CheckBox)findViewById(R.id.cbOptC);
+        cbOptC.setOnCheckedChangeListener(this);
+        cbOptD = (CheckBox)findViewById(R.id.cbOptD);
+        cbOptD.setOnCheckedChangeListener(this);
+
+        btnNext = (Button)findViewById(R.id.btnNextQuestion);
+        btnComplete = (Button)findViewById(R.id.btn_complete);
+        btnNext.setOnClickListener(this);
+        btnComplete.setOnClickListener(this);
+
+        layoutQuestions = findViewById(R.id.layout_questions);
+        layoutReport = findViewById(R.id.layout_report);
+
+        loadNextQuestion();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        switch (compoundButton.getId()) {
+            case R.id.cbOptA: {
+                break;
+            }
+            case R.id.cbOptB: {
+                break;
+            }
+            case R.id.cbOptC: {
+                break;
+            }
+            case R.id.cbOptD: {
+                break;
+            }
+            default:{
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnNextQuestion: {
+                if (btnNext.getText().toString().equalsIgnoreCase("Submit !")) {
+                    processQuestionResponse();
+                    submit();
+                } else {
+                    processQuestionResponse();
+                    currentQuestion++;
+                    loadNextQuestion();
+                }
+                if (currentQuestion == questionsList.size()) {
+                    btnNext.setText("Submit !");
+                }
+                break;
+            }
+            case R.id.btn_complete: {
+                finish();
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    private void loadNextQuestion() {
+        Question nextQ = questionsList.get(currentQuestion - 1);
+        updateQuestionsView(nextQ);
+    }
+
+    private void updateQuestionsView(Question question) {
+        resetCheckBoxes();
+        this.tvQuestion.setText(question.getQuestion());
+        this.tvOptA.setText(question.getOptions()[0]);
+        this.tvOptB.setText(question.getOptions()[1]);
+        this.tvOptC.setText(question.getOptions()[2]);
+        this.tvOptD.setText(question.getOptions()[3]);
+        updateQuestionProgress();
+    }
+
+    private void updateQuestionProgress() {
+        double size = (double) questionsList.size();
+        int progress = (int)((currentQuestion/size) * 100);
+        this.pbQuestion.setProgress(progress);
+        tvProgress.setText("Question " + currentQuestion + " of " + questionsList.size());
+    }
+
+    private void resetCheckBoxes() {
+        cbOptA.setChecked(false);
+        cbOptB.setChecked(false);
+        cbOptC.setChecked(false);
+        cbOptD.setChecked(false);
+    }
+
+    private void processQuestionResponse() {
+        Question question = questionsList.get(currentQuestion - 1);
+        boolean correct = false;
+        int[] selectedOptions = getSelectedOptions();
+        int[] rightOptions = question.getRightOptions();
+        Arrays.sort(selectedOptions);
+        Arrays.sort(rightOptions);
+        if (question.hasMultipleAnswers()) {
+            correct = true;
+            for (int opt : selectedOptions) {
+                if (!arrayContains(rightOptions, opt)) {
+                    correct = false;
+                }
+            }
+        } else {
+            if (Arrays.equals(selectedOptions, rightOptions)) {
+                correct = true;
+            }
+        }
+        scoreTracker[currentQuestion - 1] = correct;
+    }
+
+    private boolean arrayContains(int[] list, int searchKey) {
+        boolean found = false;
+        for (int item : list) {
+            if (item == searchKey) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
+    private int[] getSelectedOptions() {
+        ArrayList<Integer> selections = new ArrayList<>();
+        if (cbOptA.isChecked()) {
+            selections.add(0);
+        }
+        if (cbOptB.isChecked()) {
+            selections.add(1);
+        }if (cbOptC.isChecked()) {
+            selections.add(2);
+        }if (cbOptD.isChecked()) {
+            selections.add(3);
+        }
+        int[] options = new int[selections.size()];
+        for (int i = 0; i < selections.size(); i++) {
+            options[i] = selections.get(i);
+        }
+        return options;
+    }
+
+    private void submit() {
+        int correct = 0;
+        for (boolean c : scoreTracker) {
+            if (c) {
+                correct++;
+            }
+        }
+        tvScore.setText(correct + " / " + scoreTracker.length);
+        layoutQuestions.setVisibility(View.GONE);
+        layoutReport.setVisibility(View.VISIBLE);
     }
 }
--- app/src/main/java/com/android/elliotmill/quizapp/questions/Question.java
+++ app/src/main/java/com/android/elliotmill/quizapp/questions/Question.java
@@ -11,19 +11,27 @@ public class Question {
     private String question;
     private String[] allOptions;
     private int[] rightOptions;
-    private boolean combineRightOptions;
+    private boolean combinesRightOptions;
+    private boolean hasMultipleAnswers;
 
     public Question(String q, String[] opts, int[] rOpt) {
         super();
         this.question = q;
         this.allOptions = opts;
         this.rightOptions = rOpt;
-        this.combineRightOptions = false;
+        this.combinesRightOptions = false;
+        this.hasMultipleAnswers = false;
     }
 
     public Question(String q, String[] opts, int[] rOpt, boolean cRO) {
         this(q, opts, rOpt);
-        this.combineRightOptions = cRO;
+        this.combinesRightOptions = cRO;
+        this.hasMultipleAnswers = false;
+    }
+
+    public Question(String q, String[] opts, int[] rOpt, boolean cRO, boolean hMA) {
+        this(q, opts, rOpt, cRO);
+        this.hasMultipleAnswers = hMA;
     }
 
     public String getQuestion() {
@@ -38,9 +46,17 @@ public class Question {
         return this.rightOptions;
     }
 
+    public boolean hasMultipleAnswers() {
+        return this.hasMultipleAnswers;
+    }
+
+    public boolean combinesRightOptions() {
+        return this.combinesRightOptions;
+    }
+
     // helper method to generate all questions
-    public static List generateAllQuestions() {
-        List questions = new ArrayList<Question>();
+    public static ArrayList generateAllQuestions() {
+        ArrayList<Question> questions = new ArrayList<>();
         Question q;
         // Q1
         q = new Question("What is the first element on the periodic table ?", new String[]{"Helium", "Hydrogen", "Copper", "Oxygen"}, new int[]{1});
@@ -51,11 +67,11 @@ public class Question {
         questions.add(q);
 
         // Q3
-        q = new Question("Who discovered America ?", new String[]{"Francisco de Bobadilla", "Leif Erikson", "Christpher Columbus", "Giovanni Pallegrino"}, new int[]{1, 2});
+        q = new Question("Who discovered America ?", new String[]{"Francisco de Bobadilla", "Leif Erikson", "Christpher Columbus", "Giovanni Pallegrino"}, new int[]{1, 2}, false, true);
         questions.add(q);
 
         // Q4
-        q = new Question("Which of the following are Presidents of the United States ?", new String[]{"Alexander Hamilton", "Thomas Jefferson", "Benjamin Franklin", "James Buchanan"}, new int[]{1, 3});
+        q = new Question("Which of the following are Presidents of the United States ?", new String[]{"Alexander Hamilton", "Thomas Jefferson", "Benjamin Franklin", "James Buchanan"}, new int[]{1, 3}, true, true);
         questions.add(q);
 
         // Q5
@@ -71,15 +87,15 @@ public class Question {
         questions.add(q);
 
         // Q8
-        q = new Question("Which of the following are not fruit ?", new String[]{"Carrots", "Apples", "Aubergine", "Celery"}, new int[]{0, 2, 3}, true);
+        q = new Question("Which of the following are not fruit ?", new String[]{"Carrots", "Apples", "Aubergine", "Celery"}, new int[]{0, 2, 3}, false, true);
         questions.add(q);
 
         // Q9
-        q = new Question("What is the Formula of Dihydrogen Monoxide ?", new String[]{"H₂O", "NaCl", "H₂SO₄", "Na₂CO₃"}, new int[]{1});
+        q = new Question("What is the Formula of Dihydrogen Monoxide ?", new String[]{"H₂O", "NaCl", "H₂SO₄", "Na₂CO₃"}, new int[]{0});
         questions.add(q);
 
         // Q10
-        q = new Question("What is the Formula of Dihydrogen Monoxide ?", new String[]{"3", "1", "4", "2"}, new int[]{3});
+        q = new Question("How many electrons are in Helium ?", new String[]{"3", "1", "4", "2"}, new int[]{3});
         questions.add(q);
 
         return questions;
